5b11e11aa235a012de3ebf52ffb78d1f7ed4eb17
Michał Sochoń <kaszpir@gmail.com>
2019-12-28 23:15:13 +0100

    Initial commit for the shutdown button

---
+++ README.md
@@ -0,0 +1,54 @@
+# Raspberry Pi 3 B+ shutdown button
+
+## Requirements
+
+- Python 3
+- Raspberry Pi 3 (probably after some adjustments can work with other pi)
+
+## Usage
+
+1. install as in installation section below
+2. adjust src/rpi-power-button/power-button.py top section and systemd files
+3. restart systemd service
+4. press desired gpio button, see if if the shutdown triggers work as expected
+5. revert from DEMO mode, restart service.
+
+In default setup:
+- when rpi turns on then green led is constantly on
+- when pressing key for 2s then shutdown function is executed,
+  this will cause green led to start to blink slow, then will blink
+  faster (probably system thing), for a short time (1s?) it will
+  glow without blinking and then finally led will be totally off
+- when the green led is off it is safe to unplug the power from rpi
+
+## Installation
+
+- enable gpio in rpi3, see [luma-led-matrix docs](https://luma-led-matrix.readthedocs.io/en/latest/install.html)
+- install deps
+
+```bash
+sudo usermod -a -G spi,gpio pi
+sudo apt-get update
+sudo apt-get install -y python3-gpiozero git
+
+mkdir -p /home/pi/src
+cd /home/pi/src/
+git clone https://github.com/nvtkaszpir/rpi-power-button.git
+cd rpi-power-button
+sudo cp -f power-button.service /lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable power-button
+sudo systemctl start power-button
+```
+
+## Known limitations
+
+- adjust files to your needs
+- if your rpi is lacking power from power source then shutdown event
+  may not be triggered - make sure you shut down device before power
+  source is under certain threshold (there are special addons/hats for that)
+- your pull request may be rejected if I do not feel I need it
+
+## Other
+
+- code formatting with [black](https://github.com/psf/black)
---
+++ power-button.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+from gpiozero import Button
+from subprocess import check_call
+from signal import pause
+
+# config section start
+
+# set to True to not to really trigger system shutdown, but just print message
+DEMO = False
+
+# gpio pin to use
+BUTTON_GPIO_PIN = 27
+
+# how long to keep key pressed to trigger shutdown function
+KEY_HOLD_TIME_SECONDS = 2
+
+# which led to control, on rpi3b+ led0 is green and usually assigned to mmc
+# led1 is red and is assigned to power
+LED = "led0"
+
+# what kind of trigger to assign to led on start,
+# to see all available triggers, run sudo cat /sys/class/leds/led0/trigger
+# set to 'none' to keep it constantly bright on
+TRIGGER_START = "none"
+
+# what kind of trigger to assign to led on shutdown
+# set it to 'timer' to blink
+TRIGGER_SHUTDOWN = "timer"
+
+# config section end
+
+
+def led_control(trigger, led):
+    """Set specific led state based on trigger."""
+    # change led status to specific trigger
+    print(
+        "led_control: setting trigger: '{trigger}' for led '{led}'...".format(
+            trigger=trigger, led=led
+        )
+    )
+    os.system(
+        "echo {trigger} | sudo tee /sys/class/leds/{led}/trigger".format(
+            trigger=trigger, led=led
+        )
+    )
+
+    # sleep 1 second before another led control, otherwise it will not work
+    time.sleep(1)
+
+    # set led brightness so it is actually visible
+    print("led_control: setting brightness 1 for led '{led}'...".format(led=led))
+    os.system("echo 1 | sudo tee /sys/class/leds/{led}/brightness".format(led=led))
+
+    sys.stdout.flush()
+
+
+def shutdown(button):
+    """Run shutdown command on button pressed.
+
+    DEMO var controls if the actual shutdown command is executed.
+
+    Notice: Smut be single param passed, due to gpiozero docs.
+    """
+    print("Running shutdown due to key event on pin {}".format(button.pin))
+    sys.stdout.flush()
+
+    # set led status on shutdown
+    led_control(TRIGGER_SHUTDOWN, LED)
+
+    # run shutdown command
+    if not DEMO:
+        check_call(["sudo", "poweroff"])
+    else:
+        print("Running demo command...")
+        check_call(["sudo", "date"])
+        print("Done.")
+
+    sys.stdout.flush()
+
+
+def main():
+    """Execute main key logic.
+
+    You need to restart systemd service if you make any code changes.
+    """
+    print(
+        "Registering button to GPIO pin {}, key hold time {}s".format(
+            BUTTON_GPIO_PIN, KEY_HOLD_TIME_SECONDS
+        )
+    )
+    sys.stdout.flush()
+
+    # set led status on start
+    led_control(TRIGGER_START, LED)
+
+    # register function on key press
+    shutdown_btn = Button(BUTTON_GPIO_PIN, hold_time=KEY_HOLD_TIME_SECONDS)
+    shutdown_btn.when_held = shutdown
+
+    print("Running in demo mode: {}".format(DEMO))
+    print("Starting event loop")
+    sys.stdout.flush()  # flush output so that we can see messages in systemd
+
+    # gpiozero pause() triggers infinite loop to listen to gpio events.
+    pause()
+
+
+if __name__ == "__main__":
+    main()
